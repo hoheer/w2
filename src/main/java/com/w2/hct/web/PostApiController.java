02861884e7d0e383a761dd7fae5c187a48df9cf7
lsy <temperlooser@gmail.com>
2022-04-15 17:28:44 +0900

    2022-04-15

--- src/main/java/com/w2/hct/web/PostApiController.java
+++ src/main/java/com/w2/hct/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.w2.hct.web;
 
 import com.w2.hct.service.posts.PostsService;
+import com.w2.hct.web.Dto.PostsResponseDto;
 import com.w2.hct.web.Dto.PostsSaveRequestDto;
+import com.w2.hct.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,7 +15,17 @@ public class PostApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
